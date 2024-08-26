3b954c512b442370cb411d0b35ed3f032989b9f8
joshuaSamuel06 <samueljoshua121ny@gmail.com>
2024-08-25 20:43:00 -0400

    first commit blog

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 
 import werkzeug
@@ -14,7 +15,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, Register, Login, Comment
 from flask_gravatar import Gravatar
-
+from dotenv import load_dotenv
+load_dotenv()
 '''
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
@@ -29,7 +31,8 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+print(os.environ.get("Flask_Key"))
+app.config['SECRET_KEY'] = os.environ.get("Flask_Key")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 gravatar = Gravatar(app,
@@ -52,7 +55,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -264,4 +267,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
